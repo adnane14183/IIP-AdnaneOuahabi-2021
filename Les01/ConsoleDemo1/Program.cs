b2f1148677b11662f12760b3c779a54592ce349c
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-10-19 16:03:09 +0200

    Oefening console

---
+++ Les01/ConsoleDemo1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom!");
+            Console.Write("Wat is je naam? ");
+            string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Welkom " + name);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Druk een toest in om door te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
