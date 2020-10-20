909ca284e7126bafbb39d9b0bbfa344c45fabc5b
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-10-20 11:04:36 +0200

    oefening

---
+++ Les02/ConsoleHello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo wereld!");
+            Console.Write("Hoe heet je: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Aangename kennismaking " + name);
+            Console.WriteLine("Druk een toets op verder te gaan ...");
+            Console.ReadLine();
+
+        }
+    }
+}
