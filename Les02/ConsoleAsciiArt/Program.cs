6099b579d8354f46808307142fc35df47230d2be
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-10-20 11:39:52 +0200

    oefening

---
+++ Les02/ConsoleAsciiArt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    ##          ##");
+            Console.WriteLine("      ##      ##");
+            Console.WriteLine("    ##############");
+            Console.WriteLine("  #### ###### ####");
+            Console.WriteLine("######################");
+            Console.WriteLine("##  ##############  ##");
+            Console.WriteLine("##  ##          ##  ##");
+            Console.WriteLine("      ####  ####      ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Les02/ConsoleGedicht/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGedicht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jantje zag eens pruimen hangen,");
+            Console.ReadKey();
+            Console.WriteLine("O! als eieren zo groot.");
+            Console.ReadKey();
+            Console.WriteLine("'t scheen, dat Jantje wou gaan plukken, ");
+            Console.ReadKey();
+        }
+    }
+}
