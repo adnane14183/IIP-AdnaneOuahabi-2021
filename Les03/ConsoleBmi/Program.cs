de657b3f0072975f4f576c94131b250a4f1d2f0f
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-10-21 19:26:24 +0200

    oef

---
+++ Les03/ConsoleBmi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+            Console.Write("Lengte (in cm): ");
+            double getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Gewicht (in kg): ");
+            double getal2 = Convert.ToInt32(Console.ReadLine());
+            double deling1 = Convert.ToDouble(getal1) / 100;
+            double product = deling1 * deling1;
+            double deling = Convert.ToDouble(getal2) / Convert.ToDouble(product);
+            double deling5 = Math.Round(deling, 1);
+            Console.Write("je BMI bedraagt: " + deling5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Les03/WpfBmi/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Converters;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBmi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Double lengte;
+        Double gewicht;
+        Double total1;
+        Double total2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBmi_Click(object sender, RoutedEventArgs e)
+        {
+            lengte = Double.Parse(txtLengte.Text);
+            gewicht = Double.Parse(TxtGewicht.Text);
+
+            total1 = Convert.ToDouble(lengte) / Convert.ToDouble(100);
+            total2 = Convert.ToDouble(gewicht) / (Convert.ToDouble(total1) * total1);
+            LblBmi.Content = "Jouw BMI is " + Math.Round(total2, 1);
+
+        }
+    }
+}
