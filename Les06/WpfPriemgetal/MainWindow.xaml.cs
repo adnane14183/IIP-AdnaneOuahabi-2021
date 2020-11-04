0bcc4f92c17b537727015562934d1e20438906e2
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-11-04 14:12:01 +0100

    e

---
+++ Les06/WpfPriemgetal/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPriemgetal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnControleer_Click(object sender, RoutedEventArgs e)
+        {
+            int nummer = Convert.ToInt32(txtGetal.Text);
+            int delers = 0;
+
+            for(int i =1; i <= nummer; i++)
+            {
+                if ( nummer % i == 0)
+                {
+                    delers++;
+                }
+            }
+
+            Brush color1 = Brushes.Green;
+            Brush color2 = Brushes.Red;
+
+            if(delers == 2)
+            {
+                lblResultaat.Content = $"{nummer} is een priem getal";
+                lblResultaat.Foreground = color1;
+            }
+            else
+            {
+                lblResultaat.Content = $"{nummer} is geen priem getal";
+                lblResultaat.Foreground = color2;
+            }
+        }
+    }
+}
