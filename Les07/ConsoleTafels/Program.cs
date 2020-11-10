f7de263044abb7d83f1566f39518019d675246b3
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-11-10 11:16:50 +0100

    oef

--- Les07/ConsoleTafels/Program.cs
+++ Les07/ConsoleTafels/Program.cs
@@ -8,22 +8,23 @@ namespace ConsoleTafels
 {
     class Program
     {
-        static void Main(string[] args)
+        static private void DrukTafel(int getal, int lengte)
         {
-            int getal = 0;
-            int lengte = 0;
-
             Console.Write("geef een getal: ");
             getal = Convert.ToInt32(Console.ReadLine());
             Console.Write("geef een lengte: ");
             lengte = Convert.ToInt32(Console.ReadLine());
 
-            for ( int multiplier = 1; multiplier <= lengte; multiplier++ )
+            for (int multiplier = 1; multiplier <= lengte; multiplier++)
             {
                 Console.WriteLine("{0} * {1} = {2}", getal, multiplier, (getal * multiplier));
             }
             Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
 
         }
+            
+        }
     }
-}
