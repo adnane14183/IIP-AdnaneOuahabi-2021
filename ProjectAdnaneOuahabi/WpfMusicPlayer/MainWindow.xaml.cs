85093f914d269c4f68cd8649c6218e547865325d
adnane14183 <Adnane.Ouahabi@student.odisee.be>
2020-11-17 16:12:53 +0100

    oef

--- ProjectAdnaneOuahabi/WpfMusicPlayer/MainWindow.xaml.cs
+++ ProjectAdnaneOuahabi/WpfMusicPlayer/MainWindow.xaml.cs
@@ -31,19 +31,28 @@ namespace WpfMusicPlayer
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // laten afspelen wat er in uw listbox staat
-
             ListBoxItem selected = (ListBoxItem)ltbAfspeellijst.SelectedItem;
             player.URL = selected.Content.ToString();
 
+            // naam en artiest laten zien
+            //gevonden dankzij stackoverflow (lastindex gebruiken)
+            string name = "";
+            string artistName = ""; 
+            string songName = "";
+            name = ltbAfspeellijst.SelectedItem.ToString().Substring(ltbAfspeellijst.SelectedItem.ToString().LastIndexOf('\\')); // alles tonen na de laatste (/)
+            artistName = name.Substring(1, name.IndexOf('-')) ; //toon je alles voor (-)
+            songName = ltbAfspeellijst.SelectedItem.ToString().Substring(ltbAfspeellijst.SelectedItem.ToString().LastIndexOf('-')); //tonen van alles na de (-)
 
+            lblPlaying.Content = "playing: " + artistName + "" + songName;
+            
         }
 
         private void sldMuziek_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             //slider instellen
-            int Volumegeluid = Convert.ToInt32(Math.Round(sldMuziek.Value, 0));
-            lblVolume.Content = "Volume: " + Volumegeluid;
-            player.settings.volume = Volumegeluid;
+                int Volumegeluid = Convert.ToInt32(Math.Round(sldMuziek.Value, 0));
+                lblVolume.Content = "Volume: " + Volumegeluid;
+                player.settings.volume = Volumegeluid;
         }
 
         private void btnSelect_Click(object sender, RoutedEventArgs e)
@@ -54,54 +63,58 @@ namespace WpfMusicPlayer
 
             // filter 
             dlg.DefaultExt = ".mp3";
-
+            dlg.Filter = "MP3 Audio File (*.mp3)|*.mp3| Windows Media File (*.wma)|*.wma|WAV Audio File (*.wav)|*.wav|All Files (*.*)|*.*";
+              
             // openen
             Nullable<bool> result = dlg.ShowDialog();
 
             //  bestand in listbox zetten 
             if (result == true)
             {
-                // Open document 
-                ListBoxItem item = new ListBoxItem();
-                item.Content = dlg.FileName;
-                ltbAfspeellijst.Items.Add(item);
-
-
-
-
+                // Open document
+               
+                  ListBoxItem item = new ListBoxItem();
+                  item.Content = dlg.FileName;
+                  ltbAfspeellijst.Items.Add(item);
             }
 
         }
 
         private void btnPause_Click(object sender, RoutedEventArgs e)
         {
+            // button pause instellen
             player.controls.pause();
         }
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
+            //button afspelen instellen
             player.controls.play();
         }
 
         private void btnMute_Click(object sender, RoutedEventArgs e)
         {
+            //mute button instellen
             player.settings.mute = true;
             sldMuziek.Value = 0;
         }
 
         private void btnUnmute_Click(object sender, RoutedEventArgs e)
         {
+            //unmute button instellen
             player.settings.mute = false;
             sldMuziek.Value = 100;
         }
 
         private void btnVolgende_Click(object sender, RoutedEventArgs e)
         {
+            //volgende liedje in de afspeellijst
             ltbAfspeellijst.SelectedIndex += + 1;
         }
 
         private void btnVorige_Click(object sender, RoutedEventArgs e)
         {
+            //vorige liedje in de afspeellijst
             ltbAfspeellijst.SelectedIndex += - 1;
         }
     }
